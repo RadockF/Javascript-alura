00a767b6fdb53c87be28203f6f942e20017b3fdd
Victor Picelli <victor.picelli@cerusdomain.com>
2023-08-21 14:00:23 -0300

    funcoes e operadores

--- comparacoes.js
+++ comparacoes.js
@@ -18,3 +18,7 @@ console.log(typeof texto)
 
 Numero()
 String()
+
+// ||: Operador “ou”, retorna true caso uma condição seja válida;
+// &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+// != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false.

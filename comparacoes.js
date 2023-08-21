5f0470fafeff971102814ce0b8eea8a741a41cd9
Victor Picelli <victor.picelli@cerusdomain.com>
2023-08-21 10:56:13 -0300

    template-string, ternario e comparações

---
+++ comparacoes.js
@@ -0,0 +1,20 @@
+// == ( comparação implicita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto)
+console.log(numero === texto)
+
+//typeof - mostrar o tipo da variavel 
+
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == só compara o valor
+// === compara o valor e o tipo 
+
+// conversão explicita 
+
+Numero()
+String()
--- teste.js
+++ teste.js
@@ -131,3 +131,13 @@
 // with
 // yield
 
+
+
+// ternario com template-string 
+// const nome = "Leo";
+// const idade = 23;
+// const bebidaMaior = "cerveja";
+// const bebidaMenor = "suco";
+
+// const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+// console.log(pedido)

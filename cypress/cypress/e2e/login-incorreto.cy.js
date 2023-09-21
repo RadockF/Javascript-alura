7aa956f945ee6afeddb9880f83b439aebaa5b5ab
victor picelli <victor.picelli@gcerusbank.com.br>
2023-09-21 16:11:13 -0300

    Automatizando o teste com a api

--- cypress/cypress/e2e/login-incorreto.cy.js
+++ cypress/cypress/e2e/login-incorreto.cy.js
@@ -1,8 +1,23 @@
 describe('Página de login', () => {
+
+  beforeEach(()=> {
+  
+    cy.visit('https://3076-cypress-alurapic-front.vercel.app/#/home')
+
+    cy.intercept('POST','https://alurapic-api.onrender.com/user/login', {
+      statusCode: 401
+    }).as('stubPost')
+  
+  })
+
     it('Verificar mensagem de campos obrigatórios', () => {
-      cy.visit('https://3076-cypress-alurapic-front.vercel.app/#/home');
       cy.contains('User name is required!').should('be.visible');
       cy.contains('Password is required!').should('be.visible');
-    });
-  });
   
+    });
+
+    it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+      cy.login('victor','victor31');
+      cy.wait('@stubPost')
+  });
+})

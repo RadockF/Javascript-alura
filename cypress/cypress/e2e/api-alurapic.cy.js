723dba49b846fb7022c9891c69d624fbf3d61e64
victor picelli <victor.picelli@gcerusbank.com.br>
2023-09-22 15:59:03 -0300

    Finalizando curso de cypress

--- cypress/cypress/e2e/api-alurapic.cy.js
+++ cypress/cypress/e2e/api-alurapic.cy.js
@@ -1,14 +1,47 @@
 describe('Api Alurapic', () => {
-    it('Dados da API', () => {
-            cy.request({
-                method: 'POST',
-                url: 'https://alurapic-api.onrender.com/user/login',
-                body: Cypress.env()
-            }).then((res) => {
-                expect(res.status).to.be.equal(200)
-                expect(res.body).is.not.empty
-                expect(res.body).to.have.property('id')
-                expect(res.body.id).to.be.equal(197)
-            })
+//     it('Dados da API', () => {
+//             cy.request({
+//                 method: 'POST',
+//                 url: 'https://alurapic-api.onrender.com/user/login',
+//                 body: Cypress.env()
+//             }).then((res) => {
+//                 expect(res.status).to.be.equal(200)
+//                 expect(res.body).is.not.empty
+//                 expect(res.body).to.have.property('id')
+//                 expect(res.body.id).to.be.equal(197)
+//             })
+//     })
+// })
+
+//encontrar a foto
+// it('Fotos do usuário', () => {
+//     cy.request({
+//         method: 'GET',
+//         url: 'https://alurapic-api.onrender.com/victor/photos',
+//         body: Cypress.env()
+//     }).then((res) => {
+//         expect(res.status).to.be.equal(200)
+//         expect(res.body).is.not.empty
+//         expect(res.body[0]).to.have.property('description')
+//         expect(res.body[0].description).to.be.equal('teste')
+//     })
+// })
+// })
+
+it('Fotos do usuário', () => {
+
+    const tempoEsperado = Math.random() * 50;
+
+    cy.request({
+        method: 'GET',
+        url: 'https://alurapic-api.onrender.com/victor/photos',
+        body: Cypress.env()
+    }).then((res) => {
+        expect(res.status).to.be.equal(200)
+        expect(res.body).is.not.empty
+        expect(res.body[0]).to.have.property('description')
+        expect(res.body[0].description).to.be.equal('teste')
+        expect(res.duration).to.be.lte(tempoEsperado)
     })
+})
 })
--- cypress/cypress/e2e/cadastro-incorreto.cy.js
+++ cypress/cypress/e2e/cadastro-incorreto.cy.js
@@ -1,13 +1,21 @@
+    import Cadastro from 'C:\Users\PC\Documents\Estudos\Javascript-alura\Cypress\support\pages\cadastro\pagina-cadastro.js'
+    
     describe('Página de cadastro', () => {
-      it('Preencher os campos do formulário de forma incorreta', () => {
+      beforeEach(() => {
         cy.visit('https://3076-cypress-alurapic-front.vercel.app/#/home')
-        cy.contains('a', 'Register now').click();
-        cy.contains('button', 'Register').click();
-        cy.contains('button', 'Register').click();
-        cy.contains('Email is required!').should('be.visible');
-        cy.contains('Full name is required!').should('be.visible');
-        cy.contains('User name is required!').should('be.visible');
-        cy.contains('Password is required!').should('be.visible');
+    })
+      it('Preencher os campos do formulário de forma incorreta', () => {
+
+        // cy.get('[data-teste="register"]').click();
+        // cy.get('input[data-test="email"]').type('lllll@lll.com');
+        // cy.get('input[data-test="fullName"]').type('Lllll Lllll');
+        // cy.get('input[data-test="registerUserName"]').type('lllllllll');
+        // cy.get('input[data-test="registerPassword"]').type('LllllLllll');
+        // cy.get('[data-test="btnRegister"]').click();
+        Cadastro.acessarPaginaCadastro();
+        Cadastro.preencherFormulario();
+        Cadastro.submeterCadstro();
+
       })
     })
   
